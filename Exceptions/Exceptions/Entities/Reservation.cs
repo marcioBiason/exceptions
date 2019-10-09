5236b488ab6aea58ed49d8a3e11420439a344d44
marcBiason <54872072+marcioBiason@users.noreply.github.com>
2019-10-09 16:09:05 -0300

    Add files via upload

---
+++ Exceptions/Exceptions/Entities/Reservation.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using Exceptions.Entities.Exceptions;
+
+namespace Exceptions.Entities
+{
+    class Reservation
+    {
+        public int RoomNumber { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+
+        public Reservation()
+        {
+        }
+
+        public Reservation(int roomNumber, DateTime checkIn, DateTime checkOut)
+        {
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException(" CheckOut is bigger than checkIn ");
+            }
+
+            RoomNumber = roomNumber;
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public int Duration()
+        {
+            TimeSpan duration = CheckOut.Subtract(CheckIn);
+            return (int)duration.TotalDays;
+        }
+
+        public void UpdateDates(DateTime checkIn, DateTime checkOut)
+        {
+            DateTime now = DateTime.Now;
+
+            if (checkIn < now || checkOut < now)
+            {
+                throw new DomainException(" Update, past days ");
+            }
+
+            if (checkOut <= checkIn)
+            {
+                throw new DomainException(" CheckOut is bigger than checkIn ");
+            }
+
+            CheckIn = checkIn;
+            CheckOut = checkOut;
+        }
+
+        public override string ToString()
+        {
+            return "Room "
+                + RoomNumber
+                + ", check-in: "
+                + CheckIn.ToString("dd/MM/yyyy")
+                + ", check-out: "
+                + CheckOut.ToString("dd/MM/yyyy")
+                + ", "
+                + Duration()
+                + " nigths";
+
+        }
+    }
+}
---
+++ Exceptions/Exceptions/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Exceptions.Entities;
+using Exceptions.Entities.Exceptions;
+
+namespace Exceptions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Room Number: ");
+                int roomNumber = int.Parse(Console.ReadLine());
+
+                Console.Write("Check in date (dd/MM/yyyy): ");
+                DateTime checkIn = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Check out date (dd/MM/yyyy): ");
+                DateTime checkOut = DateTime.Parse(Console.ReadLine());
+
+                //Estanciando uma Reserva;
+                Reservation r1 = new Reservation(roomNumber, checkIn, checkOut);
+
+                //Exibindo a Reserva;
+                Console.WriteLine("Reservation " + r1);
+
+                Console.WriteLine();
+                Console.WriteLine("Enter data to update the reservation");
+
+                Console.Write("Check in date (dd/MM/yyyy): ");
+                checkIn = DateTime.Parse(Console.ReadLine());
+
+                Console.Write("Check out date (dd/MM/yyyy): ");
+                checkOut = DateTime.Parse(Console.ReadLine());
+
+                //Chamando o metodo UpdateDates da Classe Reservation
+                r1.UpdateDates(checkIn, checkOut);
+
+                //Exibindo o Update da Reserva;
+                Console.WriteLine("Update_Reservation " + r1);
+            }
+            //Classe Criada;
+            catch (DomainException e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+            //Format Exception;
+            catch(FormatException e)
+            {
+                Console.WriteLine("Format Error: " + e.Message);
+            }
+            //Erro Genérico, Pegando todos os erros;
+            catch(Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+        }
+    }
+}
